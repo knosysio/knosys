9c9115e32e17ee8c639f35fa8354e9d72d0c3f92
Ourai Lin <ourairyu@gmail.com>
2024-01-10 09:27:32 +0800

    feat(web-app): default layout style

--- packages/web-app/src/entry/layouts/default/index.tsx
+++ packages/web-app/src/entry/layouts/default/index.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import { useAppData, history, Link, Outlet } from 'umi';
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Menu } from 'antd';
+import { HomeOutlined, FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 
 import style from './style.scss';
 
 const { Header, Content, Footer, Sider } = Layout
+
+const routeIconMap: Record<string, any> = {
+  home: HomeOutlined,
+  settings: SettingOutlined,
+};
 
 function resolveMenuItems(routes = []) {
   const items = [];
@@ -22,6 +28,13 @@ function resolveMenuItems(routes = []) {
 
     if (children.length > 0) {
       resolved.children = children;
+      resolved.icon = <FileTextOutlined />
+    }
+
+    const RouteIcon = routeIconMap[route.name];
+
+    if (RouteIcon) {
+      resolved.icon = <RouteIcon />;
     }
 
     items.push(resolved);
@@ -37,38 +50,31 @@ export default function AppLayout() {
   const { clientRoutes } = useAppData();
   const { title } = process.env.KNOSYS_APP as any;
 
-  const { token: { colorBgContainer, borderRadiusLG } } = theme.useToken();
-
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        <div className={style['AppLayout-logo']}>{ collapsed ? title.slice(0, 2) : title }</div>
-        <Menu
-          theme="dark"
-          selectedKeys={menuKeys}
-          mode="inline"
-          items={resolveMenuItems(clientRoutes[0].routes)}
-          onClick={({ key }) => {
-            setMenuKeys([key]);
-            history.push(key);
-          }}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}></Header>
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
+    <Layout className={style.DefaultLayout}>
+      <Sider className={style['DefaultLayout-sidebar']} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className={style['DefaultLayout-logo']}>{ collapsed ? title.slice(0, 2) : title }</div>
+        <div className={style['DefaultLayout-navMenu']}>
+          <Menu
+            theme="dark"
+            selectedKeys={menuKeys}
+            mode="inline"
+            items={resolveMenuItems(clientRoutes[0].routes)}
+            onClick={({ key }) => {
+              setMenuKeys([key]);
+              history.push(key);
             }}
-          >
+          />
+        </div>
+      </Sider>
+      <Layout className={style['DefaultLayout-main']}>
+        <Header className={style['DefaultLayout-header']}></Header>
+        <Content className={style['DefaultLayout-body']}>
+          <div className={style['DefaultLayout-content']}>
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
+        <Footer className={style['DefaultLayout-footer']}>
           KnoSys ©{new Date().getFullYear()} Created by Ourai L.
         </Footer>
       </Layout>
